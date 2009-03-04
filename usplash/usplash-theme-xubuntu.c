4393743e71a29bcb11eea000458f4c9e0aa61381
Martin Pitt <martin.pitt@ubuntu.com>
2009-03-04 22:50:20 +0100

    * Update for new usplash which supports explicit theme sizes and pixmap   positions; see LP #64147 for details. * debian/control: Bump usplash dependencies to >= 0.5.30 (THEME_VERSION==4).

--- usplash/usplash-theme-xubuntu.c
+++ usplash/usplash-theme-xubuntu.c
@@ -53,6 +53,14 @@ struct usplash_theme usplash_theme = {
 	/* Background and font */
 	.pixmap = &pixmap_usplash_640_400,
 
+        /* theme resolution; if 0, use width/height of pixmap */
+        .theme_width = 0,
+        .theme_height = 0,
+
+        /* position of pixmap */
+        .pixmap_x = 0,
+        .pixmap_y = 0,
+
 	/* Palette indexes */
 	.background             = 0,
   	.progressbar_background = 73,
@@ -94,6 +102,14 @@ struct usplash_theme usplash_theme_640_480 = {
 
 	/* Background and font */
 	.pixmap = &pixmap_usplash_640_480,
+
+        /* theme resolution; if 0, use width/height of pixmap */
+        .theme_width = 0,
+        .theme_height = 0,
+
+        /* position of pixmap */
+        .pixmap_x = 0,
+        .pixmap_y = 0,
 
 	/* Palette indexes */
 	.background             = 0,
@@ -137,6 +153,14 @@ struct usplash_theme usplash_theme_800_600 = {
 	/* Background and font */
 	.pixmap = &pixmap_usplash_800_600,
 
+        /* theme resolution; if 0, use width/height of pixmap */
+        .theme_width = 0,
+        .theme_height = 0,
+
+        /* position of pixmap */
+        .pixmap_x = 0,
+        .pixmap_y = 0,
+
 	/* Palette indexes */
 	.background             = 0,
   	.progressbar_background = 73,
@@ -177,6 +201,14 @@ struct usplash_theme usplash_theme_1024_768 = {
 
 	/* Background and font */
 	.pixmap = &pixmap_usplash_1024_768,
+
+        /* theme resolution; if 0, use width/height of pixmap */
+        .theme_width = 0,
+        .theme_height = 0,
+
+        /* position of pixmap */
+        .pixmap_x = 0,
+        .pixmap_y = 0,
 
 	/* Palette indexes */
 	.background             = 0,
@@ -219,6 +251,14 @@ struct usplash_theme usplash_theme_1365_768_scaled = {
 	/* Background and font */
 	.pixmap = &pixmap_usplash_1365_768_scaled,
 
+        /* theme resolution; if 0, use width/height of pixmap */
+        .theme_width = 0,
+        .theme_height = 0,
+
+        /* position of pixmap */
+        .pixmap_x = 0,
+        .pixmap_y = 0,
+
 	/* Palette indexes */
 	.background             = 0,
   	.progressbar_background = 73,
@@ -255,8 +295,8 @@ struct usplash_theme usplash_theme_1365_768_scaled = {
 void t_init(struct usplash_theme *theme) {
     int x, y;
     usplash_getdimensions(&x, &y);
-    theme->progressbar_x = (x - theme->pixmap->width)/2 + theme->progressbar_x;
-    theme->progressbar_y = (y - theme->pixmap->height)/2 + theme->progressbar_y;
+    theme->progressbar_x = (x - usplash_theme_width(theme))/2 + theme->progressbar_x;
+    theme->progressbar_y = (y - usplash_theme_height(theme))/2 + theme->progressbar_y;
 }
 
 void t_clear_progressbar(struct usplash_theme *theme) {
