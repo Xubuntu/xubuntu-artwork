a186857faa51215a2aa0b7c006a1dbb07709bb40
Lionel Le Folgoc <mrpouit@ubuntu.com>
2010-02-02 22:59:47 +0100

    * New versioning scheme. * debian/control: xubuntu-gdm-theme depends on gdm... * var/lib/gdm/.gconf.defaults/%gconf-tree.xml: force gtk/icon themes, and   icons in buttons/menus. * debian/xubuntu-gdm-theme.{postrm,preinst}: handles diversion of gdm's   default gconf schema. * usr/share/xubuntu/xdg-gdm: add minimum config (xfwm4) for gdm. * debian/xubuntu-gdm-theme.postinst: make sure previous config bits are   erased on upgrades.

---
+++ usr/share/xubuntu/gdm.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright © 2010 Lionel Le Folgoc <mrpouit@ubuntu.com>
+
+# Simple script to add some fancy stuff to gdm
+
+# if not present in xdg-gdm, uses the default config
+export XDG_CONFIG_DIRS="/usr/share/xubuntu/xdg-gdm:/etc/xdg/xdg-xubuntu"
+
+# Xfconf
+if [ -x /usr/lib/xfconfd ]; then
+    /usr/lib/xfconfd &
+fi
+
+# Window manager
+if [ -x /usr/bin/xfwm4 ]; then
+    /usr/bin/xfwm4 &
+fi
+
+# and finally, power manager
+if [ -x /usr/bin/xfce4-power-manager ]; then
+    /usr/bin/xfce4-power-manager &
+fi
